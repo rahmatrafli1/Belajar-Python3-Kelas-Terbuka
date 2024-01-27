364c295d7d04709d222c4ee724ff281bfd304385
rahmatrafli1 <rahmatrafli1@gmail.com>
2024-01-27 16:08:11 +0700

    02 Pengenalan Variabel Pada Python

---
+++ 02-Variabel/main.py
@@ -0,0 +1,24 @@
+# Variabel adalah tempat menyimpan data
+
+# menaruh nilai
+
+a = 10
+b = 4 + a
+
+panjang = 10
+
+print(f"4 + {a} =",b)
+print("panjang =",panjang)
+
+# penamaan
+nilai_y = 15 # dengan menggunakan underscore
+juta10 = 10000000 # penamaan variabel angka harus dibelakang
+nilaiZ = 17.5 # camel case
+
+# pemanggilan kedua : tiban nilai variabel
+a = 7
+print("a =",a)
+
+# pemanggilan langsung dari variabel
+b = a
+print("b =",b)
