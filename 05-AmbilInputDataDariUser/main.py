5b87fe1079a1a9f4c91401eb1bca933cd3473cb0
rahmatrafli1 <rahmatrafli1@gmail.com>
2024-01-27 17:33:13 +0700

    05 Pengenalan Pengambilan Input Data Dari User Pada Python

---
+++ 05-AmbilInputDataDariUser/main.py
@@ -0,0 +1,19 @@
+# data yang dimasukkan tetap tipe nya string
+data = input("Masukkan data: ")
+print(data, ", type:",type(data))
+print()
+
+# data yang dimasukkan berupa integer
+data_int = int(input("Masukkan Angka: "))
+print(data_int, ", type:",type(data_int))
+print()
+
+# data yang dimasukkan berupa float
+data_float = float(input("Masukkan Angka: "))
+print(data_float, ", type:",type(data_float))
+print()
+
+# data yang dimasukkan berupa boolean
+data_bool = bool(int(input("Masukkan Angka: ")))
+print(data_bool, ", type:",type(data_bool))
+print()
