29c6b8fb62dcc58db9531b76c602af90d0cf6c32
rahmatrafli1 <rahmatrafli1@gmail.com>
2024-01-29 10:08:41 +0700

    08 Operasi Komparasi

---
+++ 08-OperasiKomparasi/main.py
@@ -0,0 +1,94 @@
+# >, <, >=, <=, ==, !=, is, is not
+print("Operasi Komparasi\n")
+
+# >
+print("Contoh Lebih Dari (>):\n")
+a = 4
+b = 2
+
+hasil = a > 3
+print (a,'>',3,'=',hasil)
+
+hasil = b > 3
+print (b,'>',3,'=',hasil)
+
+hasil = b > 2
+print (b,'>',2,'=',hasil)
+
+# <
+print("\nContoh Kurang Dari (<):\n")
+
+hasil = a < 3
+print (a,'<',3,'=',hasil)
+
+hasil = b < 3
+print (b,'<',3,'=',hasil)
+
+hasil = b < 2
+print (b,'<',2,'=',hasil)
+
+# >=
+print("\nContoh Lebih Dari Sama Dengan (>=):\n")
+
+hasil = a >= 3
+print (a,'>=',3,'=',hasil)
+
+hasil = b >= 3
+print (b,'>=',3,'=',hasil)
+
+hasil = b >= 2
+print (b,'>=',2,'=',hasil)
+
+# <=
+print("\nContoh Kurang Dari Sama Dengan (<=):\n")
+
+hasil = a <= 3
+print (a,'<=',3,'=',hasil)
+
+hasil = b <= 3
+print (b,'<=',3,'=',hasil)
+
+hasil = b <= 2
+print (b,'<=',2,'=',hasil)
+
+# ==
+print("\nContoh Sama Dengan (==):\n")
+
+hasil = a == 3
+print (a,'==',3,'=',hasil)
+
+hasil = b == 3
+print (b,'==',3,'=',hasil)
+
+hasil = b == 2
+print (b,'==',2,'=',hasil)
+
+# !=
+print("\nContoh Tidak Sama Dengan (!=):\n")
+
+hasil = a != 3
+print (a,'!=',3,'=',hasil)
+
+hasil = b != 3
+print (b,'!=',3,'=',hasil)
+
+hasil = b != 2
+print (b,'!=',2,'=',hasil)
+
+# is
+print("\nContoh Komparasi Object Identity (is):\n")
+x = 4
+
+hasil = a is x
+print (a,'is',x,'=',hasil)
+hasil = b is x
+print (b,'is',x,'=',hasil)
+
+# is not
+print("\nContoh Komparasi Object Identity (is not):\n")
+x = 5
+
+hasil = a is not x
+print (a,'is not',x,'=',hasil)
+hasil = b is not x
+print (b,'is not',x,'=',hasil)
